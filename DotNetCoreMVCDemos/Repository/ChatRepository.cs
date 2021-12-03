5ffb9745206ff1e08ede975820cbdbc588144a87
Hemant Jadav <mt_089@outlook.com>
2021-12-03 15:54:17 +0000

    Fixing user profile picture in contact

--- DotNetCoreMVCDemos/Repository/ChatRepository.cs
+++ DotNetCoreMVCDemos/Repository/ChatRepository.cs
@@ -494,7 +494,8 @@ public int DeleteChatMessage(int userid, int Conversationid)
                     AllUsersModel chat = new AllUsersModel
                     {
                         UserContactID = Convert.ToString(row["UserContactID"]),
-                        UserName = Convert.ToString(row["UserName"])                        
+                        UserName = Convert.ToString(row["UserName"])
+                        ProfileImage = Convert.ToString(row["ProfileImage"])                        
                     };
                     users.Add(chat);
                 }
